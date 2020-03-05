3ed2005ad4372746b91385b95900946c35f199f2
Jeff Wagner <wagnejef@mail.gvsu.edu>
2020-03-05 15:59:17 -0500

    new interface and cleanup

--- app/src/main/java/com/example/seniorproject/MainActivity.java
+++ app/src/main/java/com/example/seniorproject/MainActivity.java
@@ -7,28 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button connectButton;
+    Button disconnectButton;
+    private TextView counter;
+    private static Integer count;
 
     private static Socket socket;
     private static DataOutputStream dataOutputStream;
@@ -36,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private static StandardDeviation standardDeviation;
     private static Mean mean;
     private static String ip = "192.168.0.109";
+    private static boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +42,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        running = true;
+        count = 0;
         mean = new Mean();
         standardDeviation = new StandardDeviation();
+
         connectButton = findViewById(R.id.connectButton);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,14 +54,27 @@ public class MainActivity extends AppCompatActivity {
                 connectToServer();
             }
         });
+
+        disconnectButton = findViewById(R.id.disconnect);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                disconnect();
+            }
+        });
+
+        counter = findViewById(R.id.counter);
+        counter.setText(count.toString());
+
     }
 
-    public void connectToServer () {
+    private void connectToServer () {
+        running = true;
         serverTask task = new serverTask();
         task.execute();
     }
 
-    class serverTask extends AsyncTask<Void, Void, Void> {
+    private class serverTask extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -69,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 objectInputStream = new ObjectInputStream(socket.getInputStream());
                 dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-                while(true) {
+                while(running) {
                     double[] gene = (double[]) objectInputStream.readObject();
                     double tStat = getTStat(gene);
                     double[] permutationTStats = new double[1000];
@@ -86,7 +100,14 @@ public class MainActivity extends AppCompatActivity {
 
                     dataOutputStream.writeDouble(dScore);
                     dataOutputStream.flush();
+
+                    count++;
+                    counter.setText(count.toString());
                 }
+
+                objectInputStream.close();
+                dataOutputStream.close();
+                socket.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -94,52 +115,34 @@ public class MainActivity extends AppCompatActivity {
 
             return null;
         }
-    }
 
-    private double getTStat(double[] gene) {
-        double[] first = Arrays.copyOfRange(gene, 0, 8);
-        double firstMean = mean.evaluate(first);
-        double firstStandardDeviation = standardDeviation.evaluate(first);
+        private double getTStat(double[] gene) {
+            double[] first = Arrays.copyOfRange(gene, 0, 8);
+            double firstMean = mean.evaluate(first);
+            double firstStandardDeviation = standardDeviation.evaluate(first);
 
-        double[] last = Arrays.copyOfRange(gene, 8, gene.length);
-        double lastMean = mean.evaluate(last);
-        double lastStandardDeviation = standardDeviation.evaluate(last);
+            double[] last = Arrays.copyOfRange(gene, 8, gene.length);
+            double lastMean = mean.evaluate(last);
+            double lastStandardDeviation = standardDeviation.evaluate(last);
 
-        return (firstMean - lastMean) / Math.sqrt((Math.pow(firstStandardDeviation, 2) / 8) + Math.pow(lastStandardDeviation, 2) / 52);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return (firstMean - lastMean) / Math.sqrt((Math.pow(firstStandardDeviation, 2) / 8) + Math.pow(lastStandardDeviation, 2) / 52);
         }
 
-        return super.onOptionsItemSelected(item);
+        // helper method to shuffle permutations
+        private void shuffleArray(double[] array) {
+            int index;
+            double temp;
+            Random random = new Random();
+            for (int i = array.length - 1; i > 0; i--) {
+                index = random.nextInt(i + 1);
+                temp = array[index];
+                array[index] = array[i];
+                array[i] = temp;
+            }
+        }
     }
 
-    // helper method to shuffle permutations
-    private static void shuffleArray(double[] array) {
-        int index;
-        double temp;
-        Random random = new Random();
-        for (int i = array.length - 1; i > 0; i--) {
-            index = random.nextInt(i + 1);
-            temp = array[index];
-            array[index] = array[i];
-            array[i] = temp;
-        }
+    private void disconnect() {
+        running = false;
     }
 }
--- app/src/main/java/com/example/seniorproject/TODO.java
+++ app/src/main/java/com/example/seniorproject/TODO.java
@@ -4,11 +4,13 @@ public class TODO {
     /*
     //implement server multithreading to handle clients
     //modify server to send work
-    modify client to receive, process, and return data
+    //modify client to receive, process, and return data
     //find example problem
     //modify server to send problem lines out and receive results back (MPI style)
-    produce meaningful output
+    //produce meaningful output
     make sure clients can come and go
+        add clients to ready list on joining
+        remove initial sending
     create app ui (process counter, settings)
     make work over internet and on campus
     presentation and poster
